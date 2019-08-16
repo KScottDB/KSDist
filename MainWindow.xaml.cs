2228b71a690fdec9a1c0634e1cb883b7575fbd2c
KScott <scottbeebiwan@yahoo.com>
2019-08-15 21:04:54 -0700

    ui/classes, plaintext help file for later

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +18,47 @@ using System.Windows.Shapes;
 
 namespace KSDist
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class _Icon
+    {
+        public Bitmap NormalIcon { get; set; }
+        public Bitmap BigIcon { get; set; }
+        
+        public _Icon()
+        {
+            // unset
+            NormalIcon = KSDist.Properties.Resources.error32;
+            BigIcon = KSDist.Properties.Resources.error64;
+        }
+
+        public Stream BigIconPNG()
+        {
+            MemoryStream mem = new MemoryStream();
+            BigIcon.Save(mem, ImageFormat.Png);
+            return mem;
+        }
+        public Stream NormalIconPNG()
+        {
+            MemoryStream mem = new MemoryStream();
+            NormalIcon.Save(mem, ImageFormat.Png);
+            return mem;
+        }
+    }
+
+    public class GrigmaBalls 
+    {
+
+    }
     public partial class MainWindow : Window
     {
+        public List<string> sigma = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
